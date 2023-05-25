2865165f927d3156cc865be48c4bbd93ade70880
MARCOS <saurabh887799@gmail.com>
2023-05-25 21:52:29 +0530

    Did left work on home page

--- scholarnet/src/components/Home/Home.jsx
+++ scholarnet/src/components/Home/Home.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
-import {Heading, Stack, VStack, Text, Button, Image} from '@chakra-ui/react'
+import {Heading, Stack, VStack, Text, Button, Image, HStack, Box} from '@chakra-ui/react'
 import "./home.css";
 import { Link } from 'react-router-dom';
 import vg from "../../assets/images/img1.jpg"
+import {CgGoogle, CgYoutube} from "react-icons/cg"
+import {SiCoursera, SiUdemy} from "react-icons/si"
+import {DiAws} from "react-icons/di"
+
 
 const Home = () => {
   return (
@@ -10,27 +14,48 @@ const Home = () => {
       <div className='container'>
 
         <Stack
-        direction={["column", "row"]}
+        direction={["column", "row"]} // Phone me column else row.
         height="100%"
-        justifyContent={["center", "space-between"]}
+        justifyContent={["center", "space-between"]} // center when column, spacebeween when row.
         alignItems="center"
-        spacing={['16', '56']}
+        spacing={['16', '56']} 
         >
           <VStack width={"full"} alignItems={["center", "flex-end"]}>
-            <Heading children="Elevate Your Learning Journey with Us" size={'2xl'} />
-            <Text children="Find Valueable Content At Reasonable Price" />
+            <Heading children="Elevate Your Learning Journey with Us" size={'2xl'} /> 
+            <Text children="Find Valuable Content At Reasonable Price" />
             <Link to='/courses'>
               <Button size={"lg"} colorScheme='yellow'>
                 Explore Now !
               </Button>
             
             </Link>
-          </VStack>
+          </VStack> 
           <Image boxSize={"md"} src={vg} objectFit="contain"></Image> 
         </Stack>
 
-      </div>
-    </section>
+      </div> 
+<Box>
+
+<Heading 
+      textAlign={"center"}
+      fontFamily="body"
+      color={"yellow.400"}
+      children="OUR BRANDS" />
+
+</Box>
+ <Box>
+ <HStack>
+        <CgGoogle />
+        <CgYoutube />
+        <SiCoursera />
+        <SiUdemy />
+        <DiAws />
+      </HStack>
+
+
+ </Box>
+      
+    </section> // vg is image location upr me. 
     
   );
 }
